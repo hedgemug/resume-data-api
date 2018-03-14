7f166177692fa4c6cf99425e85a1f9e4e284f737
miteshraj <miteshraj92@gmail.com>
2018-03-13 18:12:13 -0700

    added Capstones and Experiences controller methods

--- app/controllers/v1/capstones_controller.rb
+++ app/controllers/v1/capstones_controller.rb
@@ -1,2 +1,37 @@
 class V1::CapstonesController < ApplicationController
+	def index
+		capstones = Capstone.all
+		render json: capstones.as_json
+	end
+
+	def show
+		capstone = Capstone.find(params[:id])
+		render json: capstone.as_json
+	end
+
+	def create
+		capstone = Capstone.new(
+			title: params[:title],
+			description: params[:description],
+			url: params[:url],
+			photo: params[:photo],
+			studentID: params[:student_id]
+			)
+		if capstone.save
+			render json: capstone.as_json
+		else
+			render json: {errors: "There was an error when creating your capstone"}
+		end
+	end
+
+	def update
+
+	end
+
+	def destroy
+		capstone = Capstone.find(params[:id])
+		capstone.destroy
+		render json: {message: "You have successfully deleted this capstone"}
+	end
+
 end
--- app/controllers/v1/experiences_controller.rb
+++ app/controllers/v1/experiences_controller.rb
@@ -1,2 +1,37 @@
 class V1::ExperiencesController < ApplicationController
+	def index
+		experiences = Experience.all 
+		render json: experiences.as_json
+	end
+
+	def show
+		experience = Experience.find(params[:id])
+		render json: experience.as_json
+	end
+
+	def create
+		experience = Experience.new(
+			startDate: params[:start_date],
+			endDate: params[:end_date],
+			title: params[:title],
+			company: params[:company],
+			details: params[:details],
+			studentID: params[:student_id]
+			)
+		if experience.save
+			render json: experience.as_json
+		else
+			render json: {errors: "There was an error when creating your experience"}
+	end
+
+	def update
+
+	end
+
+	def destroy
+		experience = Experience.find(params[:id])
+		experience.destroy
+		render json: {message: "You have deleted your experience"}
+	end
+
 end
